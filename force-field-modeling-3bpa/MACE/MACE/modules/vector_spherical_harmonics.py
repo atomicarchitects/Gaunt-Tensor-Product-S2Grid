a4541c3a9f9596ebd5522a4bcc8f11d8a93a2deb
Ameya Daigavane <ameya.d.98@gmail.com>
2024-11-16 13:13:34 -0500

    Fix PVSH. ChannelCombine not working.

--- force-field-modeling-3bpa/MACE/MACE/modules/vector_spherical_harmonics.py
+++ force-field-modeling-3bpa/MACE/MACE/modules/vector_spherical_harmonics.py
@@ -7,14 +7,11 @@ import e3nn
 
 def get_change_of_basis_matrix(lmax: int, scalar_interaction: bool, parity: int) -> Tuple[torch.Tensor, e3nn.o3.Irreps]:
     """Gets change of basis matrix for vector spherical harmonics."""
-    sh_ir = e3nn.o3.Irreps([(1, (1, parity))])
+    ir_on_s2 = e3nn.o3.Irreps([(1, (1, parity))])
     if scalar_interaction:
-        sh_ir += "0e"
-    tp = e3nn.o3.ReducedTensorProducts("ij", i=sh_ir, j=e3nn.o3.Irreps.spherical_harmonics(lmax))
+        ir_on_s2 = e3nn.o3.Irreps("0e") + ir_on_s2
+    tp = e3nn.o3.ReducedTensorProducts("ij", i=ir_on_s2, j=e3nn.o3.Irreps.spherical_harmonics(lmax))
 
-    # irreps = 
-    # if parity == 1:
-    #     irreps = e3nn.o3.Irreps([(mul, (ir.l, -ir.p)) for mul, ir in irreps])
     return tp.change_of_basis, tp.irreps_out
 
 
